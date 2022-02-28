086de22d32100852ff03bf600efb2efcdfe69f4e
suvendu <suvendu.das16@gmail.com>
2022-02-28 16:19:09 +0530

    updating var file

--- demoVPC/variable.tf
+++ demoVPC/variable.tf
@@ -1,5 +1,6 @@
 variable "DEP_ENV"{
 	type = string
+	description ="used to determine env"
 	validation{
 		condition = contains(["prod","dev"],lower(var.DEP_ENV))
 		error_message = "Deployment allowed in dev and prod."
